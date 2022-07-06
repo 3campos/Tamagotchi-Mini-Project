33037563fc21f026f0bef32556589933f9bf2052
ecampos <ecampos.jr54@gmail.com>
2022-07-05 21:35:47 -0500

    create class for tamagotchi with basic information to start

---
+++ js/app.js
@@ -0,0 +1,14 @@
+class tamagotchi = {
+    constructor(name, hunger, sleepiness, boredom, age){
+        this.name = name
+        this.hunger = hunger
+        this.sleepiness = sleepiness
+        this.boredom = boredom
+        this.age = age
+    }
+    evolve(){
+        //add something here to change from original image to next image
+        //start with grogu in pod, then upgrade to grogu in either chain mail or with din djarin.
+    }
+}
+
