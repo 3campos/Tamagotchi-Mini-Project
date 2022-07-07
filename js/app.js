2bb54d2f1eb98b338e33837620bb3fabcb75b25e
ecampos <ecampos.jr54@gmail.com>
2022-07-06 20:37:41 -0500

    add feeding, sleeping and play functions

--- js/app.js
+++ js/app.js
@@ -14,6 +14,30 @@ class Tamagotchi {
 
 const babyYoda = new Tamagotchi ("Grogu", 1, 1, 1, 0)
 
+function feedGrogu {
+    babyYoda.hunger -= 1
+}
+
+function makeGroguSleep {
+babyYoda.sleepiness -=1
+}
+
+function makeGroguPlay {
+    babyYoda.boredom -=1
+}
+
+//first create an array of 1-10
+//hold off: array may not be needed for enumerated values here
 
 
-// document.body.appendChild
+// document.body.appendChild (i'll append the results of my for loop that runs through the array of 1-10)
+
+
+
+//AGE
+//INCREASE PET'S AGE EVERY X MINUTES
+
+//EVENT LISTENERS FOR BUTTONS
+document.getElementById('Feed').addEventListener('click', feedGrogu)
+document.getElementById('Sleep').addEventListener('click', makeGroguSleep)
+// document.getElementById('Play').addEventListener('click', INSERT FUNCTION HERE FOR PLAYING)
